6ceb35a26e561e795e0659dedcb0426768b92f1c
lixiaopei <lixiaopei@yaochufa.com>
2017-07-19 19:15:07 +0800

    同上

--- src/Process.php
+++ src/Process.php
@@ -19,7 +19,7 @@ class Process
 
     public function start($config)
     {
-        \Swoole\Process::daemon(false,false);
+        \Swoole\Process::daemon(false);
         isset($config['swoole']['workNum']) && $this->workNum=$config['swoole']['workNum'];
 
         $this->config = $config;
